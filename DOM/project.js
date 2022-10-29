c16ad6f5975b33f7f0b7a79c2a6b59a93fb6b1fc
fotini <f.batzoni@gmail.com>
2022-10-29 11:37:05 +0300

    DOM manipulation

---
+++ DOM/project.js
@@ -0,0 +1,11 @@
+document.getElementById("h1").innerText = "Welcome to my site!";
+
+document.getElementById("p1").innerHTML = "My name is Fotini"
+
+const h2 = document.getElementById("h2");
+h2.remove()
+
+
+document.getElementById("p2").style.color ="blue"
+
+document.getElementById("p2").innerHTML = "I love JavaScript"
